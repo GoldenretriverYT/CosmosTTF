5f0a578875ad0dd4bf64a9dc3fa3a629b0e60a80
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-02-14 11:51:09 +0100

    Start CosmosMacros integration

--- CosmosTTF/TTFManager.cs
+++ CosmosTTF/TTFManager.cs
@@ -10,6 +10,35 @@ using Cosmos.System;
 
 namespace CosmosTTF {
     public static class TTFManager {
+        /*macro NumberToString aValue xResult xChars
+
+if (aValue == 0)
+{
+    xResult = "0";
+}
+else
+{
+    int xValue = aValue;
+
+    if (aValue < 0)
+    {
+        xValue *= -1;
+    }
+
+    while (xValue > 0)
+    {
+        int xValue2 = xValue % 10;
+        xResult = string.Concat(xChars[xValue2], xResult);
+        xValue /= 10;
+    }
+}
+
+if (aValue < 0)
+{
+    xResult = string.Concat("-", xResult);
+}
+*/
+        
         private static Dictionary<string, Font> fonts = new();
         private static Dictionary<string, GlyphResult> glyphCache = new();
         private static List<string> glyphCacheKeys = new();
@@ -18,6 +47,8 @@ namespace CosmosTTF {
 
         public static int GlyphCacheSize { get; set; } = 512;
         private static Canvas prevCanv;
+
+        private static string[] xChars = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
         public static void RegisterFont(string name, byte[] byteArray) {
             fonts.Add(name, new Font(byteArray));
@@ -34,7 +65,10 @@ namespace CosmosTTF {
         /// <exception cref="Exception"></exception>
         public static GlyphResult RenderGlyphAsBitmap(string font, char glyph, Color color, float scalePx = 16) {
             var rgbOffset = ((color.R & 0xFF) << 16) + ((color.G & 0xFF) << 8) + color.B;
-            var glyphCacheKey = font + glyph + scalePx + rgbOffset.ToString();
+            var rgbOffsetStr = string.Empty;
+            NumberToString(rgbOffset, rgbOffsetStr, xChars);
+            
+            var glyphCacheKey = font + glyph + scalePx + rgbOffsetStr;
 
             if (glyphCache.TryGetValue(glyphCacheKey, out GlyphResult cached))
             {
@@ -144,6 +178,10 @@ namespace CosmosTTF {
             prevCanv.DrawString(txt, Cosmos.System.Graphics.Fonts.PCScreenFont.Default, Color.White, 16, offY);
             prevCanv.Display();
         }
+
+        #region MacroDummies
+        public static void NumberToString(int aValue, string xResult, string[] xChars) { }
+        #endregion
     }
 
     public struct GlyphResult {
